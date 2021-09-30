3aa3420dbcfd0b5aaab9a04e5e8dca98d7623ad9
msteele96 <marcsteele2014@gmail.com>
2021-09-30 12:18:55 -0400

    added buttons to calculator.php

---
+++ calculator.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Malibu Calculator</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td><button class="operator">(</button></td>
+                <td><button class="operator">)</button></td>
+                <td><button class="operator">%</button></td>
+                <td><button class="operator">AC</button></td>
+            </tr>
+            <tr>
+                <td><button class="number">7</button></td>
+                <td><button class="number">8</button></td>
+                <td><button class="number">9</button></td>
+                <td><button>/</button></td>
+            </tr>
+            <tr>
+                <td><button class="number">4</button></td>
+                <td><button class="number">5</button></td>
+                <td><button class="number">6</button></td>
+                <td><button class="operator">x</button></td>
+            </tr>
+            <tr>
+                <td><button class="number">1</button></td>
+                <td><button class="number">2</button></td>
+                <td><button class="number">3</button></td>
+                <td><button class="operator">-</button></td>
+            </tr>
+            <tr>
+                <td><button>0</button></td>
+                <td><button>.</button></td>
+                <td><button class="equals">=</button></td>
+                <td><button class="operator">+</button></td>
+            </tr>
+        </table>
+    </body>
+</html>
